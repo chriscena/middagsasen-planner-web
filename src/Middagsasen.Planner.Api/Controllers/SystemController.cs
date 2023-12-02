7635f3432ce45f586d66173b8b10f9db08179314
Christoffer Cena <chris+github@cenaonline.net>
2023-12-02 16:22:28 +0100

    Diagnostikk-endpoint

---
+++ src/Middagsasen.Planner.Api/Controllers/SystemController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Middagsasen.Planner.Api.Data;
+
+namespace Middagsasen.Planner.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SystemController : ControllerBase
+    {
+        public SystemController(PlannerDbContext dbContext, InfrastructureSettings settings)
+        {
+            DbContext = dbContext;
+            Settings = settings;
+        }
+
+        public PlannerDbContext DbContext { get; }
+        public InfrastructureSettings Settings { get; }
+
+        [HttpGet]
+        public ActionResult Get()
+        {
+            DbContext.Users.Count();
+            return Ok(new { Settings.SmsSenderName});
+        }
+    }
+}
