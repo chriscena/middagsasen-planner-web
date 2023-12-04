8ec3eeabe69b5fb0d266b5c18ba7be1779fc4976
Christoffer Cena <chris+github@cenaonline.net>
2023-12-04 20:16:10 +0100

    Lagt på lukke-knapp i snackbar.

--- src/boot/notify-defaults.js
+++ src/boot/notify-defaults.js
@@ -2,4 +2,13 @@ import { Notify } from "quasar";
 
 Notify.setDefaults({
   color: "indigo-10",
+  actions: [
+    {
+      icon: "close",
+      color: "primary",
+      round: true,
+      flat: true,
+      size: "sm",
+    },
+  ],
 });
