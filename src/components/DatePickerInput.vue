b9a092adaa01a98c5d6ad4b2b63395878a5855cb
Christoffer Cena <chris+github@cenaonline.net>
2023-12-15 18:18:30 +0100

    Lagt til date- og timepicker.

---
+++ src/components/DatePickerInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <q-input
+    outlined
+    inputmode="numeric"
+    hide-bottom-space
+    mask="##.##.####"
+    placeholder="DD.MM.ÅÅÅÅ"
+    @focus="(event) => (event.target?.select ? event.target.select() : _)"
+    :model-value="props.modelValue"
+    @update:model-value="(val) => emit('update:model-value', val)"
+    ><template v-slot:append>
+      <q-icon name="event" class="cursor-pointer">
+        <q-popup-proxy transition-show="scale" transition-hide="scale">
+          <q-date
+            :model-value="props.modelValue"
+            @update:model-value="(val) => emit('update:model-value', val)"
+            mask="DD.MM.YYYY"
+          >
+            <div class="row items-center justify-end">
+              <q-btn v-close-popup label="Lukk" color="primary" flat />
+            </div>
+          </q-date>
+        </q-popup-proxy>
+      </q-icon> </template
+  ></q-input>
+</template>
+
+<script setup>
+const props = defineProps({
+  modelValue: {
+    type: String,
+    default: undefined,
+  },
+});
+const emit = defineEmits(["update:model-value"]);
+</script>
