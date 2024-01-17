1ea211dfd165d89f050a9da3d59dd6782cf0ff49
Christoffer Cena <chris+github@cenaonline.net>
2024-01-17 23:48:26 +0100

    Kun telle vakt per event. Ikkje begrense til top 50.

--- Views/HallOfFame.sql
+++ Views/HallOfFame.sql
@@ -1,6 +1,7 @@
 create view HallOfFame as
-  select top 50 u.UserId, u.FirstName, u.LastName, count(eru.EventResourceUserId) Shifts  from Users u
+  select u.UserId, u.FirstName, u.LastName, count(distinct er.EventId) Shifts  from Users u
   join EventResourceUsers eru on eru.UserId = u.UserId
+  join EventResources er on er.EventResourceId = eru.EventResourceId
   Where eru.EndTime < cast(cast(GETDATE() as date) as datetime)
   GROUP by u.UserId, u.FirstName, u.LastName
   order by Shifts desc, u.FirstName
