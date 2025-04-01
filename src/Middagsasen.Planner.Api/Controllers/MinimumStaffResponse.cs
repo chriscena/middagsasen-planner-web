0ddbcaee9b43ff9f265261e406f5cb683bdf7fe1
RubenHCena <122569473+RubenHCena@users.noreply.github.com>
2025-04-01 16:36:22 +0200

    #25 legge til tumme vakter Api (#51)
    Fixes #25
    ---------
    
    Co-authored-by: Ruben Hersleth Cena <rubencena+github@icenas.net>

---
+++ src/Middagsasen.Planner.Api/Controllers/MinimumStaffResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace Middagsasen.Planner.Api.Controllers
+{
+    public class MinimumStaffResponse
+    {
+        public int MinimumStaff { get; set; }
+        public int EventResourceId { get; set; }
+    }
+}
