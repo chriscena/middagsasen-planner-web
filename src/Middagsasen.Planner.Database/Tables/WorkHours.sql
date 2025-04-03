51f69d048914e66ea39c96b12ac1326f84ecc2b0
Ruben Hersleth Cena <rubencena+github@cenas.net>
2025-04-03 09:59:02 +0200

    WorkHours table

---
+++ src/Middagsasen.Planner.Database/Tables/WorkHours.sql
@@ -0,0 +1,17 @@
+CREATE TABLE WorkHours
+(
+  WorkHourId INT not null identity, 
+  constraint PK_WorkHours PRIMARY key(WorkHourId),
+  UserId INT not null,
+  constraint FK_WorkHours_Users_UserId foreign key (UserId) REFERENCES Users(UserId),
+  ShiftId INT null,
+  constraint FK_WorkHours_Users_ShiftId foreign key (ShiftId) REFERENCES EventResourceUsers(EventResourceUserId),
+  StartTime DATETIME not null, 
+  EndTime DATETIME null,
+  Description NVARCHAR(max) null,
+  ApprovedBy INT null,
+  constraint FK_WorkHours_Users_ApprovedBy foreign key (ApprovedBy) REFERENCES Users(UserId),
+  ApprovedTime DATETIME null,
+  IsDeleted BIT constraint DF_WorkHours_IsDeleted DEFAULT 0,
+
+)
